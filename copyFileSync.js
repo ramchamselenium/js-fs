46dc2bc14be2915480d2f091218d512ca72eab8c
Ramcham T Gaid <ramcham_gaid@yahoo.com>
2023-04-25 00:37:11 +0800

    copyFileSync
    copyFileSync

---
+++ copyFileSync.js
@@ -0,0 +1,14 @@
+import { copyFileSync, constants, rm } from 'node:fs';
+
+function callback(err) {
+    if (err) throw err;
+    console.log('source.txt was copied to destination.txt');
+  }
+
+// destination.txt will be created or overwritten by default.
+copyFileSync('source.txt', 'deletedestination.txt');
+console.log('source.txt was copied to destination.txt');
+
+rm('Deletedestination.txt', callback)
+// By using COPYFILE_EXCL, the operation will fail if destination.txt exists.
+//copyFileSync('source.txt', 'deletedestination.txt', constants.COPYFILE_EXCL);
