29b48086f637898bc4521e6828fd69388cd8792d
Ramcham T Gaid <ramcham_gaid@yahoo.com>
2023-04-26 03:05:51 +0800

    createReadStream.js
    createReadStream.js

--- createReadStream.js
+++ createReadStream.js
@@ -1,18 +1,18 @@
 import { open } from 'node:fs/promises';
 
-const fd = await open('/dev/input/event0');
-// Create a stream from some character device.
-const stream = fd.createReadStream();
-setTimeout(() => {
-  stream.close(); // This may not close the stream.
-  // Artificially marking end-of-stream, as if the underlying resource had
-  // indicated end-of-file by itself, allows the stream to close.
-  // This does not cancel pending read operations, and if there is such an
-  // operation, the process may still not be able to exit successfully
-  // until it finishes.
-  stream.push(null);
-  stream.read(0);
-}, 100);
+const fd = await open('source.txt');
+fd.createReadStream({ start: 90, end: 99 });
 
-// const fd = await open('sample.txt');
-// fd.createReadStream({ start: 90, end: 99 });
+// const fd = await open('/dev/input/event0');
+// // Create a stream from some character device.
+// const stream = fd.createReadStream();
+// setTimeout(() => {
+//   stream.close(); // This may not close the stream.
+//   // Artificially marking end-of-stream, as if the underlying resource had
+//   // indicated end-of-file by itself, allows the stream to close.
+//   // This does not cancel pending read operations, and if there is such an
+//   // operation, the process may still not be able to exit successfully
+//   // until it finishes.
+//   stream.push(null);
+//   stream.read(0);
+// }, 100);
